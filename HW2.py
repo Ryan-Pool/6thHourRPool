314f4d9aff2bd93103c359dae574150ce4926853
Ryan Pool <rp125328@student.wpsok.org>
2024-08-22 13:38:19 -0500

    Added comments, print statements, inputs, and integers

---
+++ HW2.py
@@ -0,0 +1,12 @@
+#Ryan Pool
+#6th Hour
+#HW2
+
+print("Hello World")
+
+name = input("What is your name? ")
+print("Hello",name)
+x = int(input("Give me a number: "))
+y = int(input("Give me another number: "))
+z = x + y
+print(z)
