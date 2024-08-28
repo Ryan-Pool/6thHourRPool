093054739ab286b6e335a1a9803b14e34e25ae33
Ryan Pool <rp125328@student.wpsok.org>
2024-08-28 14:02:35 -0500

    Added Commments, print statements, lists, sum of num  lists

---
+++ HW3.py
@@ -0,0 +1,21 @@
+#Ryan Pool
+#6th Hour
+#HW3
+
+print("Hello World")
+nameList = ["Colin", "Ryan", "Kyle", "CJ", "Cayde-6"]
+nameList.append("Ikora")
+
+numList = [5, 7, 1, 2]
+numList.insert(5, 8)
+
+print(f"{nameList[4]}")
+print(numList)
+
+numList.sort()
+print(numList)
+
+sum = numList[0] + numList[1] + numList[2]
+print(sum)
+
+
