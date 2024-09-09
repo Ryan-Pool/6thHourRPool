7e22cd207845da9a87dc96a5c45fca0e65c9e05b
Ryan-Pool <rp125328@student.wpsok.org>
2024-09-09 11:04:00 -0500

    Added unique answers to if user answers yes, no, Yes, No.

--- Playground.py
+++ Playground.py
@@ -5,10 +5,33 @@
 #The conversation between user and computer leading up to the game
 userName = input("What is your name? ")
 print("Hello",userName)
-input("Would you like to play Rock, Paper, Scissors? ")
 
-#Asking which will user choose?
-userChoice = input("You'll play anyway. So, Rock Paper or Scissors? ").lower()
+#Computer asking if user wants to play r p s
+userResponse = input("Would you like to play Rock, Paper, Scissors? ")
+
+#If user puts yes or no computer's response:
+def determineAnswer(userResponse):
+    if userResponse == 'Yes' or \
+        userResponse == 'yes':
+        return "Awesome! Please enter rock, paper, or scissors:"
+    elif userResponse == 'No' or \
+        userResponse == 'no':
+        return "Too bad you will play anyway. Please enter rock, paper, or scissors:"
+    else:
+        return "I didn't understand that, but let's play anyway! Please enter rock, paper, or scissors:"
+
+#print the computers response
+response = determineAnswer(userResponse)
+print(response)
+
+#user choice for r p s
+userChoice = input().lower()
+
+#Code to make users choice valid
+choices = ['rock', 'paper', 'scissors']
+while userChoice not in choices:
+    print("invalid choice, please enter rock, paper, or scissors:")
+    userChoice = input().lower()
 
 #Random code function (The code for the computer picking random r p s)
 import random
