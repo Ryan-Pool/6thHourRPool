11256cff3bf1eb78359c3111885693321ced0f6f
Ryan Pool <rp125328@student.wpsok.org>
2024-10-24 13:52:25 -0500

    Finished HW9

---
+++ HW9.py
@@ -0,0 +1,20 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW9
+
+#1. Print Hello World!
+print("Hello World!")
+
+#2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
+#https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
+
+temperature = int(input("What is the temperature? "))
+
+if temperature > 20:
+    print("It's hot :)")
+elif temperature > 10:
+        print("Its Mild :)")
+else:
+    print("It's cold :)")
+
+print("Thank You!❤️")
