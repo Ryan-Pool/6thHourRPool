cde8a652a90dd0af160479e5486e8dc77d8a3452
Ryan Pool <rp125328@student.wpsok.org>
2025-01-31 13:38:54 -0600

    HW15 edited and HW19 finished

--- HW15.py
+++ HW15.py
@@ -7,11 +7,8 @@ def helloWorld():
     print("Hello World!")
 
 #2. Create a def function that calculates the average of three numbers.
-a = 2
-b = 3
-c = 7
 
-def averageNum():
+def averageNum(a, b, c):
     print((a + b + c)/3)
 
 #3. Create a def function with the names of 5 animals as arguments, treats it like a list, and
@@ -19,22 +16,14 @@ def averageNum():
 def animalList(*name):
     print("The 3rd animal is", name[2])
 
-animalList("Capybara", "Manta Ray", "Whale Shark", "Rat", "Quokka")
 
 #4. Create a def function that loops from 1 to the number put in the argument.
 def numberCount(x):
     i = 1
 
-    while i < x:
+    while i <= x:
         print(i)
         i += 1
 
-
-x = int(input("Please enter a number: "))
-numberCount(x)
-
 #5. Call all of the functions created in 1 - 4 with relevant arguments.
 
-numberCount(x)
-
-animalList(*name)
---
+++ HW19.py
@@ -0,0 +1,14 @@
+#Name:
+#Class: 6th Hour
+#Assignment: HW19
+
+#1. Import all the functions from the HW15 file.
+from HW15 import helloWorld, averageNum, animalList, numberCount
+
+#2. Call the functions here and run HW19.
+helloWorld()
+averageNum(2, 3, 7)
+animalList("Capybara", "Manta Ray", "Whale Shark", "Rat", "Quokka")
+numberCount(int(input("Please enter a number: ")))
+
+#3. Delete all calls from HW15 and run HW19 again.
