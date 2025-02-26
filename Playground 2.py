78d8b208853b4076f1ecb3f6a6051f7dd09d5036
Ryan Pool <rp125328@student.wpsok.org>
2025-02-26 13:32:10 -0600

    Edited Playground code

--- Playground 2.py
+++ Playground 2.py
@@ -1,11 +1,3 @@
-# The conversation between you and the computer leading up to the game
-userName = input("What is your name? ")
-print("Hello", userName)
-input("Would you like to play Rock, Paper, Scissors? ")
-
-# Asking which will you choose?
-userChoice = input("You'll play anyway. So, Rock, Paper, or Scissors? ").lower()
-
 # Random code function (The code for the computer picking random r p s)
 import random
 
@@ -15,8 +7,36 @@ choices = ['rock', 'paper', 'scissors']
 # Computer choice
 computerChoice = random.choice(choices)
 
+# The conversation between you and the computer leading up to the game
+userName = input("What is your name? ")
+print("Hello", userName)
+
+#Computer asking if user wants to play r p s
+userResponse = input("Would you like to play Rock, Paper, Scissors? ")
+
+#Add code for if user inputs no or yes
+def determineAnswer(userResponse):
+    if userResponse == 'Yes' or \
+        userResponse == 'yes':
+        return "Awesome! Please enter rock, paper, or scissors:"
+    elif userResponse == 'No' or \
+        userResponse == 'no':
+        return "Too bad you will play anyway. Please enter rock, paper, or scissors:"
+    else:
+        return "I didn't understand that, but let's play anyway! Please enter rock, paper, or scissors:"
+
+#print the computers response
+response = determineAnswer(userResponse)
+print(response)
+
+# Asking which will you choose? Uses try except to throw up error if user does not enter from choices.
+userChoice = input()
+
+if not type(userChoice) is choices:
+    raise Exception("Sorry, you can only enter rock, paper, or scissors.")
+else:
 # Response to the "which will you choose?" question
-print(f"I chose: {computerChoice}")
+    print(f"I chose: {computerChoice}")
 
 # Code for if the user won or the computer won.
 def determineWinner(user, computer):
